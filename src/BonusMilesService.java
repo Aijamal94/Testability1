293cf112293871bef5df0894ac13a47e855a224f
Aizhamal <aijashka79@gmail.com>
2024-05-06 18:15:29 +0500

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,7 @@
-public class BonusMilesSevice {
+public class BonusMilesService {
 
-    public static void main(String[] args) {
-        public int calculate;
-        (int ticketPrice)
+    public int calculate(int ticketPrice) {
         int milesPer20Rubles = 1; // Количество миль за каждые 20 рублей
         return ticketPrice / 20 * milesPer20Rubles;
-
     }
 }
