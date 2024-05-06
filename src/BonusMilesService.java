2da645196f63f8ba2dccf9b0dbbd5dbf5cfac5d4
Aizhamal <aijashka79@gmail.com>
2024-05-06 18:06:35 +0500

    Update .gitignore to ignore logs directory

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesSevice {
+
+    public static void main(String[] args) {
+        public int calculate;
+        (int ticketPrice)
+        int milesPer20Rubles = 1; // Количество миль за каждые 20 рублей
+        return ticketPrice / 20 * milesPer20Rubles;
+
+    }
+}
